4ab3ac5124e2e2871bf736f47352e3dad4d92e86
Carol St.Louis <stlouis_c@yahoo.com>
2016-05-03 18:08:26 -0700

    added checkbox to template and event handlers and component state to allow hiding of tasks checked as completed

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -37,6 +37,12 @@ import Task from './Task.jsx';
 // }
 
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			hideCompleted: false,
+		};
+	}
 	handleSubmit(event){
 		event.preventDefault();
 
@@ -51,8 +57,22 @@ class App extends Component {
 		ReactDOM.findDOMNode(this.refs.textInput).value='';
 	}
 
+	toggleHideCompleted(){
+		this.setState({
+			hideCompleted: !this.state.hideCompleted,
+		});
+	}
+
 	renderTasks(){
-		return this.props.tasks.map((task) => (
+		// deprecated after switch to filteredTasks to allow hiding of completed tasks
+		// return this.props.tasks.map((task) => (
+		// 	<Task key={task._id} task={task} />
+		// ));
+		let filteredTasks = this.props.tasks;
+		if(this.state.hideCompleted){
+			filteredTasks = filteredTasks.filter(task => !task.checked);
+		}
+		return filteredTasks.map((task) => (
 			<Task key={task._id} task={task} />
 		));
 	}
@@ -61,6 +81,15 @@ class App extends Component {
 			<div className="container">
 				<header>
 					<h1>ToDo List</h1>
+					<label className="hide-completed">
+						<input 
+							type="checkbox"
+							readOnly
+							checked={this.state.hideCompleted}
+							onClick={this.toggleHideCompleted.bind(this)}
+						/>
+						Hide Completed Tasks
+					</label>
 					<form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
 						<input
 							type="text"
