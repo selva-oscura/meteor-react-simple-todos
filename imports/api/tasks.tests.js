2383aa6d2fe3f1d60ab49d87df0f1511748a6fc9
Carol St.Louis <stlouis_c@yahoo.com>
2016-05-06 20:10:57 -0700

    added test that attempt to delete other user's task will throw 'not-authorized' error

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -1,8 +1,11 @@
 /* eslint-env-mocha */
 
+// run via command line: 
+// meteor test --driver-package practicalmeteor:mocha
+
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
-import { assert } from 'meteor/practicalmeteor:chai';
+import { assert, expect } from 'meteor/practicalmeteor:chai';
 import { Tasks } from './tasks.js';
 
 
@@ -10,6 +13,7 @@ if(Meteor.isServer){
 	describe('Tasks', () => {
 		describe('methods', () => {
 			const userId = Random.id();
+			const otherUserId = Random.id();
 			let taskId;
 			beforeEach(() => {
 				Tasks.remove({});
@@ -20,19 +24,35 @@ if(Meteor.isServer){
 					username: 'blahblah'
 				});
 			});
-			it('can delete owned task', () => {
-				// Find the internal implementation of the task method in order to test in isolation
-				const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+			describe('deletion tests', () => {
+				it('can\'t delete other user\'s task (should throw not-authorized error)', () => {
+					// Find the internal implementation of the task method in order to test in isolation
+					const deleteTask = Meteor.server.method_handlers['tasks.remove'];
 
-				// set up fake method invocation consistent with what the method expect
-				const invocation = { userId };
+					// set up fake method invocation consistent with non-owner id
+					const invocation = { otherUserId };
 
-				// Run method with 'this' set to fake invocation
-				deleteTask.apply(invocation, [taskId]);
+					// Run method to test if error
+					expect(deleteTask.bind(invocation, taskId)).to.throw(Meteor.Error('not-authorized'));
 
-				// verify method does what expected
-				assert.equal(Tasks.find().count(), 0);
-			})
-		})
+					// verify method does what expected
+					assert.equal(Tasks.find().count(), 1);
+
+				});
+				it('can delete owned task', () => {
+					// Find the internal implementation of the task method in order to test in isolation
+					const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+					// set up fake method invocation consistent with what the method expect
+					const invocation = { userId };
+
+					// Run method with 'this' set to fake invocation
+					deleteTask.apply(invocation, [taskId]);
+
+					// verify method does what expected
+					assert.equal(Tasks.find().count(), 0);
+				});
+			});
+		});
 	});
 }
